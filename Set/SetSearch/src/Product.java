8d3526810ab7a25edf5cbbc908ba796ad9073653
Matheus Besson <matheusbesson.main@gmail.com>
2025-07-11 19:19:16 -0300

    Comparator and comparable

--- Set/SetSearch/src/Product.java
+++ Set/SetSearch/src/Product.java
@@ -1,6 +1,35 @@
-public class Product {
-    private int code;
+public class Product implements Comparable<Product>{
+
+    private int id;
     private String name;
     private double price;
     private int quantity;
+
+    public Product(int id, String name, double price, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    @Override
+    public int compareTo(Product o) {
+        return name.compareToIgnoreCase(o.getName());
+    }
 }
--- Set/SetSearch/src/ProductsRegister.java
+++ Set/SetSearch/src/ProductsRegister.java
@@ -1,12 +1,30 @@
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class ProductsRegister {
-    Set<Product> productList;
+    Set<Product> productSet;
 
     public ProductsRegister() {
-        this.productList = new HashSet<>();
+        this.productSet = new HashSet<>();
     }
 
+    public void addProduct(int id, String name, double price, int quantity) {
+        productSet.add(new Product(id, name, price, quantity));
+        System.out.println("Product added to the set.");
+    }
+
+    public Set<Product> productsByName() {
+        Set<Product> productByName = new TreeSet<>(productSet);
+        return productByName;
+    }
+
+    public Set<Product> productsByPrice() {
+        Set<Product> productsByPrice = new TreeSet<>(new ComparatorByPrice());
+        productSet.addAll(productSet);
+        return productsByPrice;
+    }
 
 }
